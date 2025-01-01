e888662357b8b2244f13b0ec23462cc3cb66c48f
sarfraz-web <mdsarfraz4499@gmail.com>
2025-01-02 00:34:19 +0530

    navbar border fix

--- components/ui/hero-highlight.tsx
+++ components/ui/hero-highlight.tsx
@@ -29,7 +29,7 @@ export const HeroHighlight = ({
   return (
     <div
       className={cn(
-        "relative h-[10rem] flex items-center dark:bg-black justify-center w-full group",
+        "relative h-[10rem] flex items-center dark:bg-black justify-center w-full group mt-20",
         containerClassName
       )}
       onMouseMove={handleMouseMove}
