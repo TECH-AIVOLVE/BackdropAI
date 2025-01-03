c895a26b961ed66f529e6c74a05f3b576d78aaf6
Md Adnan Baqi <adnan.baki786@gmail.com>
2025-01-03 07:25:48 +0530

    ref : head up

--- components/additional-info.tsx
+++ components/additional-info.tsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import React from "react";
 import { WobbleCard } from "./ui/wobble-card";
-
+//
 import Link from "next/link";
 
 export function AdditionalInfo() {
